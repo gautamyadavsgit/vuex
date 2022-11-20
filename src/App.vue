debc91552f76c43bde945790d6a2f80ca7e03296
Gautam Yadav <82496188+gautamyadavsgit@users.noreply.github.com>
2022-11-20 11:57:35 +0530

    project setup

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <base-container title="Vuex">
+   <the-counter></the-counter>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+
+export default {
+  components: {
+    TheCounter,
+    BaseContainer,
+  },
+  
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/TheCounter.vue
@@ -0,0 +1,20 @@
+<template>
+    <h3>{{ counter }}</h3>
+  <button @click="addOne">Add 1</button>
+</template>
+
+<script>
+export default{
+    computed:{
+   counter(){
+    return this.$store.state.counter;
+   }
+  },
+  methods:{
+    addOne(){
+      console.log(this.$store.state.counter++);
+    }
+  }
+
+}
+</script>
